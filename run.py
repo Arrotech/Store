ca67efe22f25faffe4b1efdeee37a952adc957bb
Harun Gachanja <harungachanjagitundu@gmail.com>
2021-11-29 21:42:31 +0300

    First commit for heroku

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import os
 from app import create_app
+from flask import url_for, redirect
+from app.api.v1 import store_v1
 
 config_name = os.environ.get('FLASK_ENV')
 app = create_app(config_name)
+
+@app.route('/')
+def index():
+    return redirect(url_for('store_v1.index'))
 
 if __name__ == '__main__':
     app.run()
